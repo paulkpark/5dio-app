b19939715f525e5c4d7a81faf1699a6137ba75fd
paulkpark <36182959+paulkpark@users.noreply.github.com>
2025-10-23 00:20:57 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-
-window.SUPABASE_URL   = "https://xdjgumqdwedgzwqturcx.supabase.co";
-window.SUPABASE_KEY   = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhkamd1bXFkd2VkZ3p3cXR1cmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MDQzNDUsImV4cCI6MjA3NjA4MDM0NX0.pZcnU1xMaaBBdvytSTVrLPsLU9r_3FPzCPSUeBFUsaU";
-window.SUPABASE_BUCKET = "media";
-window.SUPABASE_PUBLIC_BASE =
-  window.SUPABASE_URL.replace(/\/+$/,'') + "/storage/v1/object/public/" + window.SUPABASE_BUCKET;
